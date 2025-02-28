5109e204e02f143c3f16fc380099b5d8ff1e0d66
Mohaned <mohandmohamed104@gmail.com>
2025-02-28 14:46:41 +0200

    Intialize the repo

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+function Home() {
+  return (
+    <>
+      <h1>Welcome Home</h1>
+      <button
+        className="px-4 py-2 text-whitesm:px-8 sm:py-3 rounded-3xl bg-sky-700 hover:bg-sky-800 ">
+        Do Nothing
+      </button>
+    </>
+  );
+}
+
+export default Home;
