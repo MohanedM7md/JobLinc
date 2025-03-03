dcb069d0e63259c7e31a75b698807149253d875c
Mohaned <mohandmohamed104@gmail.com>
2025-03-03 23:27:16 +0200

    added colors theme and working on dark mode functions

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode:"class",
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        CrimsonRed/*(Primary) */:"#D72638",    /* Main buttons, highlights, brand color */
+        DarkBurgundy:"#780000",   /* Navbar, headers, strong contrast sections */
+        SoftRosewood:"#A52A2A",    /* Secondary buttons, borders, accents */
+        LightGray:"#F4F4F4",    /* Backgrounds, cards, subtle UI elements */
+        WarmWhite:"#FAFAFA",    /* Main background, clean space */
+        CharcoalBlack:"#222222",    /* Main text, high contrast for readability */
+        MutedSilver:"#A0A0A0",    /* Secondary text, icons, minor details */
+      }
+    },
   },
   plugins: [],
 };
