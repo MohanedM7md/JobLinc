1ecc4aabf24e67d3098f050a91d725fbe52d661b
Mohaned <mohandmohamed104@gmail.com>
2025-03-11 22:04:58 +0200

    Added UserProfile Component

---
+++ src/components/UserProfile.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Img } from "react-image";
+interface UserProfileProps {
+  userId: string;
+  children: React.ReactNode;
+}
+
+function UserProfile({ children }: UserProfileProps) {
+  return <div className={``}>{children}</div>;
+}
+
+UserProfile.Image = ({
+  photoUrl,
+  userName,
+  className,
+}: {
+  photoUrl: string;
+  userName: string;
+  className?: string;
+}) => (
+  <Img src={photoUrl} alt={userName} className={`rounded-full ${className}`} />
+);
+
+UserProfile.Name = ({
+  name,
+  className,
+}: {
+  name: string;
+  className?: string;
+}) => <h1 className={`font-medium text-sm ${className}`}>{name}</h1>;
+
+UserProfile.Headline = ({
+  headline,
+  className,
+}: {
+  headline: string;
+  className?: string;
+}) => <p className={`text-xs text-gray-500 ${className}`}>{headline}</p>;
