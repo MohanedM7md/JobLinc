ebc76bbfdf15fdc1b5da6886a2dc02b1d9d3808a
a7medsoltan2004@gmail.com <a7medsoltan2004@gmail.com>
2025-03-14 02:07:47 +0200

    api directory deleted

--- src/api/NothingFetch.tsx
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export const GetNothing = () =>{
-    axios
-    .get("https://jsonplaceholder.typicode.com/todos/1")
-    .then(data => console.log(data))
-    .catch(err => console.log(err));
-}
