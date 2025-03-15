47015d629e8d8755e0742f204b1250bd982c5720
Darkosylver <darkosylver27@gmail.com>
2025-03-15 22:45:48 +0200

    API mocking is now ONLINE

--- src/components/Posts/PostContainer.tsx
+++ src/components/Posts/PostContainer.tsx
@@ -1,9 +1,16 @@
 import Post from "./PostCard";
 import { postsResponse } from "../../__mocks__/PostsMock/postsDB";
+import axios from "axios";
+import { useEffect } from "react";
 
 
 export default function PostContainer() {
   const posts = postsResponse;
+  const fetchData = async () => {
+    const response = await axios.get("http://localhost:5173/api/post/feed/");
+    console.log(response.data);
+  };
+  useEffect(() => {fetchData()}, []);
   return (
     <div className="flex flex-wrap lg:w-5/12 md:w-8/12 sm:1/1 m-auto">
       {posts.map((post, i) => {
