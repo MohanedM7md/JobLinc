237e03c408cb18bc198eb9ca04f527eb8abc2f17
Darkosylver <darkosylver27@gmail.com>
2025-03-15 16:39:02 +0200

    Comment and Reply GUIs are ready

--- src/components/Posts/PostCard.tsx
+++ src/components/Posts/PostCard.tsx
@@ -26,7 +26,8 @@ export default function Post(props: PostProps) {
         userID: "1",
         firstName: "Anime",
         lastName: "Protagonist",
-        profilePicture: "",
+        profilePicture:
+          "https://i.pinimg.com/550x/04/bb/21/04bb2164bbfa3684118a442c17d086bf.jpg",
         headline: "I am the main character",
         commentText:
           "A very good comment, yes indeed, It is called Lothric, where the transitory lands of the Lords of Cinder converge.",
