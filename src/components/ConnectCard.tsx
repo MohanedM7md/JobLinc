a112b951b59f63e59c1237f4730387ae57e63cef
a7medsoltan2004@gmail.com <a7medsoltan2004@gmail.com>
2025-03-15 13:27:28 +0200

    merge resolved

--- src/components/ConnectCard.tsx
+++ src/components/ConnectCard.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import UserProfile from "./UserProfile";
 interface ProfilePictureProps {
     profilePicture: string;
     userName:string;
@@ -15,6 +16,11 @@ const handleClick = () => {
     return(
         <div className="border-2 border-gray-200 rounded-xl flex flex-col w-70 h-80 justify-center items-center">
             <div className="relative flex flex-col w-full h-7/20 justify-center items-center">
+                {/* <UserProfile.Image
+                photoUrl={props.profilePicture}
+                userName={props.userName}
+                className="absolute w-29 h-29 rounded-full top-7/20 object-cover shadow-sm"
+                /> */}
                 <img src={props.profilePicture}
                 alt= "Profile Photo" 
                 className="absolute w-29 h-29 rounded-full top-7/20 object-cover shadow-sm"></img>
@@ -30,7 +36,7 @@ const handleClick = () => {
                 <p className="">mutuals will apear here</p>
                 <button
                 onClick={handleClick}
-                className={`border-2 px-15 py-0.5 ${isClicked ? "text-darkGray border-darkGray" : "text-crimsonRed border-crimsonRed"} rounded-full font-semibold hover:bg-lightGray`}>
+                className={`border-2 px-15 py-0.5 ${isClicked ? "text-darkGray border-darkGray" : "text-crimsonRed border-crimsonRed"} rounded-full font-semibold hover:bg-lightGray hover:border-3`}>
                 <i className={`${isClicked ? "fa-regular fa-clock":"fa-solid fa-user-plus"} mr-1`}></i>  
                 {isClicked ? "Pending" : "Linc"}
                 </button>
