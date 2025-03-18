0729a7043989d551912539ec16cc42a844c31f2e
Mohaned <mohandmohamed104@gmail.com>
2025-03-18 21:21:25 +0200

    added the remaining alias of the repo folders

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,12 @@ export default defineConfig({
   resolve: {
     alias: {
       "@chatComponent": path.resolve(__dirname, "src/components/chat/*"),
+      "@context": path.resolve(__dirname, "src/context/*"),
+      "@hooks": path.resolve(__dirname, "src/hooks/*"),
+      "@pages": path.resolve(__dirname, "src/pages/*"),
+      "@services": path.resolve(__dirname, "src/services/*"),
+      "@utils": path.resolve(__dirname, "src/utils/*"),
+      "@store": path.resolve(__dirname, "src/store/*"),
     },
   },
   server: {
