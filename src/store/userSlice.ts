4a7be40c2a93eee79b1b7adf8e39fb06fa72cc1a
1Zatona1 <omar.ashrafofficial1@gmail.com>
2025-03-24 18:04:11 +0200

    Removed any remainders of setting the forgot, reset, and refresh token in userSlice.ts

--- src/store/userSlice.ts
+++ src/store/userSlice.ts
@@ -6,7 +6,6 @@ interface UserState {
   userId: number | null;
   role: string | null;
   accessToken: string | null;
-  refreshToken: string | null;
   confirmed: boolean | null;
   status: "IDLE" | "LOADING" | "SUCCESS" | "FAILED";
   loggedIn: boolean;
@@ -19,7 +18,6 @@ const initialState: UserState = {
   status: "IDLE",
   loggedIn: false,
   accessToken: null, // leave it as it is
-  refreshToken: null, // Local Storage
 
 };
 
@@ -174,7 +172,6 @@ const userSlice = createSlice({
       state.status = "IDLE";
       state.loggedIn = false;
       state.accessToken = null;
-      state.refreshToken = null; {/* TO BE REMOVED WHEN HTTP COOKIE IS AVAILABLE*/}
     },
   },
   extraReducers: (builder) => {
@@ -190,7 +187,6 @@ const userSlice = createSlice({
           state.userId = userData.userID || null;
           state.role = userData.role || null;
           state.accessToken = userData.accessToken || null;
-          // state.refreshToken = userData.refreshToken || null;
           localStorage.setItem("refreshToken", userData.refreshToken);
           state.confirmed = userData.confirmed || null;
           state.status = "SUCCESS";
@@ -214,7 +210,6 @@ const userSlice = createSlice({
           state.role = userData.role || null;
           state.accessToken = userData.accessToken || null;
           localStorage.setItem("refreshToken", userData.refreshToken);
-          // state.refreshToken = userData.refreshToken || null;
           state.confirmed = userData.confirmed || false;
           state.status = "SUCCESS";
           state.loggedIn = true;
@@ -232,7 +227,6 @@ const userSlice = createSlice({
 
         if (userData)
         {
-          // state.forgotToken = userData.forgotToken || null;
           state.status = "SUCCESS"
         }
       })
@@ -266,7 +260,6 @@ const userSlice = createSlice({
           state.userId = userData.userId || null;
           state.role = userData.role || null;
           state.accessToken = userData.accessToken || null;
-          // state.refreshToken = userData.refreshToken || null;
           localStorage.setItem("refreshToken", userData.refreshToken);
           state.status = "SUCCESS";
         }
@@ -284,7 +277,6 @@ const userSlice = createSlice({
         if (userData)
         {
           state.accessToken = userData.accessToken || null;
-          // state.refreshToken = userData.refreshToken || null;
           localStorage.setItem("refreshToken", userData.refreshToken);
           state.status = "SUCCESS";
         }
@@ -344,7 +336,6 @@ const userSlice = createSlice({
           state.userId = userData.userID;
           state.role = userData.role;
           state.accessToken = userData.accessToken;
-          // state.refreshToken = userData.refreshToken;
           localStorage.setItem("refreshToken", userData.refreshToken);
           state.confirmed = userData.confirmed;
         }
