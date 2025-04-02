0cd004621e393ec97c77dc8f35baab17fd5e211e
Mohaned <mohandmohamed104@gmail.com>
2025-04-02 13:02:07 +0200

    fixed chat card updates

--- src/services/api/api.ts
+++ src/services/api/api.ts
@@ -14,7 +14,6 @@ export const api = axios.create({
 api.interceptors.request.use(
   (config) => {
     const token = store.getState().user.accessToken;
-    console.log("accessToken : ", token);
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
