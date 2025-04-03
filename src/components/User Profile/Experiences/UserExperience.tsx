8a870d4aa472c39e55d428e26cea6e5db7740994
Darkosylver <darkosylver27@gmail.com>
2025-04-03 22:32:55 +0200

    Proper date printing

--- src/components/User Profile/Experiences/UserExperience.tsx
+++ src/components/User Profile/Experiences/UserExperience.tsx
@@ -1,22 +1,34 @@
 import { ExperienceInterface } from "interfaces/userInterfaces";
 
 interface ExperienceProps {
-    experience: ExperienceInterface
+  experience: ExperienceInterface;
 }
 
+export default function UserExperience(props: ExperienceProps) {
+  const formattedStartDate = new Date(
+    props.experience.startDate,
+  ).toLocaleDateString("en-US", { month: "short", year: "numeric" });
+  const formattedEndDate = props.experience.endDate
+    ? new Date(props.experience.endDate).toLocaleDateString("en-US", {
+        month: "short",
+        year: "numeric",
+      })
+    : "Present";
 
-
-export default function UserExperience(props:ExperienceProps) {
-    return (
-      <div className="flex flex-row mx-1">
-        <div className="flex flex-col">
-          <span className="font-medium -m-0.5">{props.experience.position}</span>
-          <span className="text-sm -m-0.5 mt-0.5">{props.experience.company}</span>
-          <span className="text-sm -m-0.5 text-mutedSilver">
-            {props.experience.startDate.toString()}
-          </span>
-          <span className="text-sm my-3 -mx-0.5">{props.experience.description}</span>
-        </div>
+  return (
+    <div className="flex flex-row mx-1">
+      <div className="flex flex-col">
+        <span className="font-medium -m-0.5">{props.experience.position}</span>
+        <span className="text-sm -m-0.5 mt-0.5">
+          {props.experience.company}
+        </span>
+        <span className="text-sm -m-0.5 text-mutedSilver">
+          {formattedStartDate} - {formattedEndDate}
+        </span>
+        <span className="text-sm my-3 -mx-0.5">
+          {props.experience.description}
+        </span>
       </div>
-    );
+    </div>
+  );
 }
