f457c9f1a7c12654f3f4caa97ee96f28a3d0ad19
Mohaned <mohandmohamed104@gmail.com>
2025-04-17 12:27:48 +0200

    delted unwanted files

--- src/App.tsx
+++ src/App.tsx
@@ -60,63 +60,71 @@ function App() {
             <Route path="/reset-password" element={<ResetPassword />} />
           </Route>
 
-          {/*   <Route element={<ProtectedRoute />}> */}
-          <Route element={<Layout />}>
-            <Route path="/home" element={<Home />} />
-            <Route path="/my-network" element={<MyNetwork />} />
-            <Route path="/connections" element={<Connections />} />
-            <Route path="/messaging" element={<Messaging />} />
-            <Route path="/change-password" element={<ChangePassword />} />
-            <Route path="/confirm-email" element={<ConfirmEmail />} />
+          <Route element={<ProtectedRoute />}>
+            <Route element={<Layout />}>
+              <Route path="/home" element={<Home />} />
+              <Route path="/my-network" element={<MyNetwork />} />
+              <Route path="/connections" element={<Connections />} />
+              <Route path="/messaging" element={<Messaging />} />
+              <Route path="/change-password" element={<ChangePassword />} />
+              <Route path="/confirm-email" element={<ConfirmEmail />} />
 
-            <Route path="/settings" element={<Settings />}>
-              <Route index element={<AccountPreferences />}></Route>
+              <Route path="/settings" element={<Settings />}>
+                <Route index element={<AccountPreferences />}></Route>
 
-              {/* Account Preferences Pages */}
-              <Route
-                path="account-preferences"
-                element={<AccountPreferences />}
-              />
-              <Route
-                path="account-preferences/display/dark-mode"
-                element={<DarkMode />}
-              ></Route>
-              <Route path="account-preferences/account-management/close-account" element={<CloseAccount />} />
+                {/* Account Preferences Pages */}
+                <Route
+                  path="account-preferences"
+                  element={<AccountPreferences />}
+                />
+                <Route
+                  path="account-preferences/display/dark-mode"
+                  element={<DarkMode />}
+                ></Route>
+                <Route
+                  path="account-preferences/account-management/close-account"
+                  element={<CloseAccount />}
+                />
 
-              <Route
-                path="sign-in-security"
-                element={<SignInAndSecurity />}
-              ></Route>
-              <Route path="sign-in-security/account-access/change-password" element={<ChangePassword />} />
+                <Route
+                  path="sign-in-security"
+                  element={<SignInAndSecurity />}
+                ></Route>
+                <Route
+                  path="sign-in-security/account-access/change-password"
+                  element={<ChangePassword />}
+                />
 
-              <Route path="visibility" element={<Visibility />}></Route>
-              <Route path="data-privacy" element={<DataAndPrivacy />}></Route>
-              <Route
-                path="advertising-data"
-                element={<AdvertisingData />}
-              ></Route>
-              <Route path="notifications" element={<Notifications />}></Route>
-            </Route>
+                <Route path="visibility" element={<Visibility />}></Route>
+                <Route path="data-privacy" element={<DataAndPrivacy />}></Route>
+                <Route
+                  path="advertising-data"
+                  element={<AdvertisingData />}
+                ></Route>
+                <Route path="notifications" element={<Notifications />}></Route>
+              </Route>
 
-            <Route path="/update-email" element={<UpdateEmail />} />
-            <Route path="/update-username" element={<UpdateUsername />} />
-            <Route path="/in" element={<UserProfile />}></Route>
-            <Route path="/profile/:userId">
-              <Route index element={<ProfileContainer />} />
-              <Route path="details/experiences" element={<FullExperiences />} />
-              <Route
-                path="details/certificates"
-                element={<FullCertificates />}
-              />
-              <Route path="details/skills" element={<FullSkills />} />
-            </Route>
-            <Route path="/post">
-              <Route path="create" element={<PostCreate />} />
-              <Route path=":postId/edit" element={<PostEdit />} />
+              <Route path="/update-email" element={<UpdateEmail />} />
+              <Route path="/update-username" element={<UpdateUsername />} />
+              <Route path="/in" element={<UserProfile />}></Route>
+              <Route path="/profile/:userId">
+                <Route index element={<ProfileContainer />} />
+                <Route
+                  path="details/experiences"
+                  element={<FullExperiences />}
+                />
+                <Route
+                  path="details/certificates"
+                  element={<FullCertificates />}
+                />
+                <Route path="details/skills" element={<FullSkills />} />
+              </Route>
+              <Route path="/post">
+                <Route path="create" element={<PostCreate />} />
+                <Route path=":postId/edit" element={<PostEdit />} />
+              </Route>
             </Route>
           </Route>
-
-          {/* </Route> */}
 
           <Route path="*" element={<Error404 />} />
         </Routes>
