82c86699d3e072cfa5db3e4806a0171715dae058
Darkosylver <darkosylver27@gmail.com>
2025-05-02 23:35:18 +0300

    link supports in comments and replies

--- src/components/Posts/Comments/CommentContent.tsx
+++ src/components/Posts/Comments/CommentContent.tsx
@@ -1,11 +1,19 @@
+import FormattedPostText from "../FormattedPostText";
+
 interface CommentContentProps {
-    commentText: string;
+  commentText: string;
 }
 
-export default function CommentContent(props : CommentContentProps) {
-    return (
-        <div className="flex flex-col w-1/1">
-            <p className="text-sm text-wrap text-charcoalBlack mx-4.5">{props.commentText}</p>
-        </div>
-    )
+export default function CommentContent(props: CommentContentProps) {
+  return (
+    <div className="flex flex-col w-1/1">
+      <p className="text-sm text-wrap text-charcoalBlack mx-4.5">
+        <FormattedPostText
+          text={props.commentText}
+          taggedUsers={[]}
+          taggedCompanies={[]}
+        />
+      </p>
+    </div>
+  );
 }
