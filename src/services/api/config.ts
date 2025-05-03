57ece80bf2a1d15bf22ca61956789062b4fe129a
Darkosylver <darkosylver27@gmail.com>
2025-05-03 10:57:43 +0300

    fixing post card counters, correct company routing

--- src/services/api/config.ts
+++ src/services/api/config.ts
@@ -1,4 +1,4 @@
-const SERVER_URL: string = "http://localhost:3000";
+const SERVER_URL: string = "https://joblinc.me:6969";
 export const API_URL: string = `${SERVER_URL}/api/`;
 export const CLOUDINARY_BASE_URL =
   "https://res.cloudinary.com/dufshbyse/image/upload/v1744191130";
