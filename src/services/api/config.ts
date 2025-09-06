8e706e65b03d82a23b41068cd9d8492d8cd162da
Mohaned Mohamed <125491698+MohanedM7md@users.noreply.github.com>
2025-09-06 20:41:29 +0300

    Update config.ts

--- src/services/api/config.ts
+++ src/services/api/config.ts
@@ -1,4 +1,4 @@
-const SERVER_URL: string = "https://joblincbe-production.up.railway.app";
+const SERVER_URL: string = "https://joblincbe.onrender.com";
 export const API_URL: string = `${SERVER_URL}/api/`;
 export const CLOUDINARY_BASE_URL =
   "https://res.cloudinary.com/dufshbyse/image/upload/v1744191130";
